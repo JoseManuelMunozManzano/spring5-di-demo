b78c956c738d0db04a66e15cc0dc76bcffa3464e
Neimerc <Neil_Mercury@yahoo.es>
2019-08-31 13:13:44 +0200

    Adding Context

---
+++ src/main/java/com/neimerc/didemo/controllers/MyController.java
@@ -0,0 +1,13 @@
+package com.neimerc.didemo.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public String hello() {
+        System.out.println("Hello!!!");
+
+        return "foo";
+    }
+}
